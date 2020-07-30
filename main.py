65ddbc8c732982a4a862451b48e89d07e79dda97
Devan Lawal <devan.lawal@gmail.com>
2020-07-30 16:55:49 +0000

    I switched my png image to Ryu I gave gravity to the image Tried to give it movement

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
 import os
 
-im_path = os.path.join("Kamui.png")
+im_path = os.path.join("RyUU.png")
 
-class Kamui(object):
+class RyUU(object):
   def __init__(self):
     pygame.sprite.Sprite.__init__(self)
 
-    Kamui.image = pygame.image.load("Kamui.png") 
+    RyUU.image = pygame.image.load("RyUU.png") 
     #Above you are loading in the image
-    self.image = Kamui.image
+    self.image = RyUU.image
+    self.image = pygame.transform.scale(self.image,(150,100))
+
           #transform is pygame exclusive
 
     self.x = 50
@@ -21,8 +23,21 @@ class Kamui(object):
     surface.blit(self.image, (self.x, self.y))
 #This function is used to draw the image onto the background
 #You need this because the computer can't draw it itself
+  def movement(self, width, height):
+    key = pygame.key.get_pressed()
 
+    if key[pygame.K_DOWN] and self.y < height - 50:
+      self.y += 1
+    if key[pygame.K_UP] and self.y > 0:
+      self.y -= 2
+    if key[pygame.K_LEFT] and self.x > 0:
+      self.x -= 1
+    if key[pygame.K_RIGHT] and self.x < width - 50:
+      self.x += 1
 
+def Gravity(self, height):
+  if self.y < height - 50 and pygame.key.get_focused():
+    self.y += 1
 
 
 pygame.init()
@@ -30,7 +45,7 @@ screen_width = 600
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-Sprite = Kamui()  
+Sprite = RyUU()  
 clock = pygame.time.Clock()
 
 running = True
@@ -41,10 +56,13 @@ while running:
       running = False
   
   screen.fill((255,255,255))
+  Sprite.Gravity(screen_height  )
   Sprite.draw(screen)
 
   pygame.display.update()
   
   clock.tick(60)
+
+
 
 
